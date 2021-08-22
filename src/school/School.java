60fee9727980979eaa1b3b9850c6de88b47393c1
eunnb <eunnb.dev@gmail.com>
2021-08-22 22:35:15 +0900

    🎉첫 커밋

---
+++ src/school/School.java
@@ -0,0 +1,49 @@
+package school;
+
+import java.util.ArrayList;
+
+//싱글톤 패턴
+public class School {
+
+    private static School instance = new School();
+
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+    private ArrayList<Subject> subjectList = new ArrayList<Subject>();
+
+    private School() {}
+
+    public static School getInstance() {
+        if( instance == null)
+            instance = new School();
+
+        return instance;
+    }
+
+    public static void setInstance(School instance) {
+        School.instance = instance;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public ArrayList<Subject> getSubjectList() {
+        return subjectList;
+    }
+
+    public void setSubjectList(ArrayList<Subject> subjectList) {
+        this.subjectList = subjectList;
+    }
+
+    public void addStudent(Student student) {
+        studentList.add(student);
+    }
+
+    public void addSubject(Subject subject) {
+        subjectList.add(subject);
+    }
+}
---
+++ src/school/Subject.java
@@ -0,0 +1,51 @@
+package school;
+
+import utils.Define;
+
+import java.util.ArrayList;
+
+public class Subject {
+
+    private int subjectId;
+    private String subjectName;
+    private int gradeType;
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public Subject(int subjectId, String subjectName) {
+        this.subjectId = subjectId;
+        this.subjectName = subjectName;
+        this.gradeType = Define.AB_TYPE;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getGradeType() {
+        return gradeType;
+    }
+
+    public void setGradeType(int gradeType) {
+        this.gradeType = gradeType;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+}
